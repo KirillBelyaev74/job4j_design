bf42659879430fe5b3847d44982cefa11ac2059e
KirillBelyaev74 <kirbel74@gmail.com>
2020-08-11 23:34:43 +0500

    2. Анализ доступности сервера.[859#263247]

--- chapter_002/src/main/java/ru/job4j/io/Analizy.java
+++ chapter_002/src/main/java/ru/job4j/io/Analizy.java
@@ -1,41 +1,26 @@
 package ru.job4j.io;
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Analizy {
 
     public void unavailable(String source, String target) throws IOException {
-        List<String> listResult = new ArrayList<>();
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(source))) {
-            bufferedReader.lines().forEach(listResult::add);
+        boolean value = false;
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(source));
+             PrintWriter printWriter = new PrintWriter(new BufferedOutputStream(new FileOutputStream(target)))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                if (!value & (line.contains("400") || line.contains("500"))) {
+                    printWriter.write(line.substring(line.indexOf(" ")));
+                    printWriter.write(System.lineSeparator());
+                    value = true;
+                } else  if (value && (!line.contains("400") && !line.contains("500"))) {
+                    printWriter.write(line.substring(line.indexOf(" ")));
+                    printWriter.write(System.lineSeparator());
+                    value = false;
+                }
+            }
         } catch (FileNotFoundException fnfe) {
             fnfe.printStackTrace();
-        }
-        this.recordMistake(this.searchMistake(listResult), target);
-    }
-
-    public List<String> searchMistake(List<String> listWithMistake) {
-        int index = 0;
-        while (index != listWithMistake.size()) {
-            String line = listWithMistake.get(index);
-            if (!line.contains("400") && !line.contains("500") && index == 0) {
-                listWithMistake.remove(index);
-            } else if ((line.contains("400") || line.contains("500")) &&
-                    (listWithMistake.get(index + 1).contains("400") || listWithMistake.get(index + 1).contains("500"))) {
-                listWithMistake.remove(index + 1);
-            } else {
-                index++;
-            }
-        }
-        return listWithMistake;
-    }
-
-    public void recordMistake(List<String> mistake, String target) {
-        try (PrintWriter printWriter = new PrintWriter(new BufferedOutputStream(new FileOutputStream(target)))) {
-            mistake.forEach(s -> printWriter.write(s + "\r"));
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
 
