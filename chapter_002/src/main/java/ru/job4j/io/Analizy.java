86f2ede7238d41f180cbd6e91e3da16c0839742e
KirillBelyaev74 <kirbel74@gmail.com>
2020-08-12 23:38:11 +0500

    2. Анализ доступности сервера.[859#263247]

--- chapter_002/src/main/java/ru/job4j/io/Analizy.java
+++ chapter_002/src/main/java/ru/job4j/io/Analizy.java
@@ -1,30 +1,38 @@
 package ru.job4j.io;
 import java.io.*;
+import java.util.*;
 
 public class Analizy {
 
-    public void unavailable(String source, String target) throws IOException {
+    public void findMistakes(String source, String target) throws IOException {
         boolean value = false;
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(source));
-             PrintWriter printWriter = new PrintWriter(new BufferedOutputStream(new FileOutputStream(target)))) {
+        List<String> list = new LinkedList<>();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(source))) {
             String line;
             while ((line = bufferedReader.readLine()) != null) {
                 if (!value & (line.contains("400") || line.contains("500"))) {
-                    printWriter.write(line.substring(line.indexOf(" ")));
-                    printWriter.write(System.lineSeparator());
+                    list.add(line.substring(line.indexOf(" ") + 1) + System.lineSeparator());
                     value = true;
                 } else  if (value && (!line.contains("400") && !line.contains("500"))) {
-                    printWriter.write(line.substring(line.indexOf(" ")));
-                    printWriter.write(System.lineSeparator());
+                    list.add(line.substring(line.indexOf(" ") + 1) + System.lineSeparator());
                     value = false;
                 }
             }
         } catch (FileNotFoundException fnfe) {
             fnfe.printStackTrace();
         }
+        this.recordMistakes(target, list);
+    }
+
+    public void recordMistakes(String target, List<String> list) {
+        try (PrintWriter printWriter = new PrintWriter(new BufferedOutputStream(new FileOutputStream(target)))) {
+            list.forEach(printWriter::write);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) throws IOException {
-        new Analizy().unavailable("with_errors.csv", "without_errors.csv");
+        new Analizy().findMistakes("with_errors.csv", "without_errors.csv");
     }
 }
