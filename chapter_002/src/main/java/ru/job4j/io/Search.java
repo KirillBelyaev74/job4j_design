4a3cda2599648bfd1892ff2accfb447f8a33aa5d
KirillBelyaev74 <kirbel74@gmail.com>
2020-08-28 13:16:34 +0500

    5. Валидация параметров запуска.[246865#263241]

--- chapter_002/src/main/java/ru/job4j/io/Search.java
+++ chapter_002/src/main/java/ru/job4j/io/Search.java
@@ -1,18 +1,33 @@
 package ru.job4j.io;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.*;
 import java.util.List;
 
 public class Search {
 
-    public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, "java").forEach(System.out::println);
-    }
-
     public static List<Path> search(Path root, String ext) throws IOException {
         SearchFiles searchFiles = new SearchFiles(ext);
         Files.walkFileTree(root, searchFiles);
         return searchFiles.getPaths();
+    }
+
+    public static void start(String[] args) throws IOException {
+        validate(args);
+        Path start = Paths.get(args[0]);
+        search(start, args[1]).forEach(System.out::println);
+    }
+
+    public static void validate(String[] args) throws FileNotFoundException {
+        if (args[1] == null) {
+            throw new IllegalArgumentException();
+        }
+        if (!Files.exists(Paths.get(args[0]))) {
+            throw new FileNotFoundException();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        start(args);
     }
 }
