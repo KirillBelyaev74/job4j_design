cc4a015329aac84d4f9077498adf3e1b9958b71e
KirillBelyaev74 <kirbel74@gmail.com>
2020-10-26 12:05:33 +0500

    0. Видео[6863#263173]

--- chapter_003/src/main/java/ru/job4j/jdbc/ConnectToBD.java
+++ chapter_003/src/main/java/ru/job4j/jdbc/ConnectToBD.java
@@ -1,19 +1,23 @@
 package ru.job4j.jdbc;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Properties;
+import java.io.*;
+import java.sql.*;
+import java.util.*;
 
 public class ConnectToBD {
     public static void main(String[] args) {
-        Properties property = new Properties();
-        try {
-            FileInputStream fis = new FileInputStream("C:\\project\\job4j_design\\chapter_003\\src\\main\\resources\\config.properties");
-            property.load(fis);
-            String host = property.getProperty("db.host");
-            String login = property.getProperty("db.login");
-            String password = property.getProperty("db.password");
-            System.out.println("HOST: " + host + ", LOGIN: " + login + ", PASSWORD: " + password);
-        } catch (IOException e) {
+        try (InputStream inputStream = ImportDB.class.getClassLoader().getResourceAsStream("config.properties")) {
+            Properties properties = new Properties();
+            properties.load(inputStream);
+            String url = properties.getProperty("db.url");
+            String username = properties.getProperty("db.username");
+            String password = properties.getProperty("db.password");
+            System.out.println("HOST: " + url + ", LOGIN: " + username + ", PASSWORD: " + password);
+            try (Connection connection = DriverManager.getConnection(url, username, password)) {
+                DatabaseMetaData metaData = connection.getMetaData();
+                System.out.println(metaData.getUserName());
+                System.out.println(metaData.getURL());
+            }
+        } catch (IOException | SQLException e) {
             System.err.println("ОШИБКА: Файл свойств отсуствует!");
         }
     }
