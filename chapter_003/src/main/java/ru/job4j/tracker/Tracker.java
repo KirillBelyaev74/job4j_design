90e7a7570ff65481dbeef373e73168867ad4f11c
KirillBelyaev74 <kirbel74@gmail.com>
2020-10-26 13:29:52 +0500

    validate

--- chapter_003/src/main/java/ru/job4j/tracker/Tracker.java
+++ chapter_003/src/main/java/ru/job4j/tracker/Tracker.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class Tracker implements Store{
+public class Tracker implements Store {
     /**
      * Массив для хранение заявок.
      */
