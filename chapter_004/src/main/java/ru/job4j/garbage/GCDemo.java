6d8f80db604c864d732720c1e56a75a6f48d2807
Kirill Belyaev <38278255+KirillBelyaev74@users.noreply.github.com>
2020-11-09 12:39:58 +0500

    Демонстрация работы GC[1589#263286]

--- chapter_004/src/main/java/ru/job4j/garbage/GCDemo.java
+++ chapter_004/src/main/java/ru/job4j/garbage/GCDemo.java
@@ -6,23 +6,23 @@ public class GCDemo {
     private static final long MB = KB * KB;
     private static final Runtime ENVIRONMENT = Runtime.getRuntime();
 
+
     public static void info() {
         final long freeMemory = ENVIRONMENT.freeMemory();
         final long totalMemory = ENVIRONMENT.totalMemory();
         final long maxMemory = ENVIRONMENT.maxMemory();
         System.out.println("=== Environment state ===");
-        System.out.printf("Free: %d%n", freeMemory / MB);
-        System.out.printf("Total: %d%n", totalMemory / MB);
-        System.out.printf("Max: %d%n", maxMemory / MB);
+        System.out.printf("Free: %d%n", freeMemory);
+        System.out.printf("Total: %d%n", totalMemory);
+        System.out.printf("Max: %d%n", maxMemory);
     }
 
     public static void main(String[] args) {
         info();
-        for (int i = 0; i < 20; i++) {
-            new Person(i, "N" + i);
+        for (int index = 0; index != 10; index++) {
+            new Person(index, "index " + index);
         }
         System.gc();
         info();
     }
-
 }
--- chapter_004/src/main/java/ru/job4j/garbage/Person.java
+++ chapter_004/src/main/java/ru/job4j/garbage/Person.java
@@ -5,6 +5,9 @@ public class Person {
     private int age;
     private String name;
 
+    public Person() {
+    }
+
     public Person(int age, String name) {
         this.age = age;
         this.name = name;
