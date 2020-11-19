2aedf1938fdee17930a6dae056e47a93a7cb9c56
Kirill Belyaev <38278255+KirillBelyaev74@users.noreply.github.com>
2020-11-19 16:53:53 +0500

    3. Шаблонизатор.[855#263261]

---
+++ chapter_005/src/test/java/ru/job4j/tdd/GeneratorStringTest.java
@@ -0,0 +1,37 @@
+package ru.job4j.tdd;
+
+import org.junit.Test;
+import java.util.Map;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class GeneratorStringTest {
+
+    private final String template = "I am a ${name}, Who are ${subject}?";
+
+    @Test
+    public void whenTemplateThenString() {
+        Generator generator = new GeneratorString();
+        Map<String, String> args = Map.of("name", "Kirill Belyaev", "subject", "you");
+        String result = generator.produce(this.template, args);
+        assertThat(result, is("I am a Kirill Belyaev, Who are you?"));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void whenTemplateThenNullPointerException() {
+        Generator generator = new GeneratorString();
+        Map<String, String> args = Map.of("surname", "Kirill Belyaev", "object", "you");
+        String result = generator.produce(this.template, args);
+        assertThat(result, is(args));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenTemplateThenIllegalArgumentException() {
+        Generator generator = new GeneratorString();
+        Map<String, String> args = Map.of(
+                "name", "Kirill Belyaev", "subject", "you",
+                "surname", "Kirill Belyaev", "object", "you");
+        String result = generator.produce(this.template, args);
+        assertThat(result, is(args));
+    }
+}
